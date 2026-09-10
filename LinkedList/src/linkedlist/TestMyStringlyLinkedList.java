dcb9d61bf4ca20c9195c2298826dc1e415612a4a
Trịnh Huy Hoàng <trinhhuyhoang04122005@gmail.com>
2026-09-10 21:12:32 +0700

    test chương trình

--- LinkedList/src/linkedlist/TestMyStringlyLinkedList.java
+++ LinkedList/src/linkedlist/TestMyStringlyLinkedList.java
@@ -3,6 +3,19 @@ package linkedlist;
 public class TestMyStringlyLinkedList {
 
     public static void main(String[] args) {
+        MyStringlyLinkedList MSLL = new MyStringlyLinkedList();
+        Person p = new Person("Alex", 10);
+        MSLL.addLast(p);
+        String[] names = {"Victoria", "John", "Anna", "Dinh Bac"};
+        int[] ages = {19, 20, 21, 22};
+        MSLL.addMany(names, ages);
+        MSLL.traverse();
+        if (MSLL.searchByName("Alex") == null) {
+            System.out.println("Not found!");
+        } else {
+            System.out.println("Found!");
+        }
+        System.out.println(MSLL.isEmpty());
     }
 
 }
