795f19f2c9cbc82aef10b3dc312b48b6d4ad2118
Trịnh Huy Hoàng <trinhhuyhoang04122005@gmail.com>
2026-09-21 16:28:47 +0700

    Hoan thanh ArrayQueue

--- LinkedList/src/myqueue/ArrayQueue.java
+++ LinkedList/src/myqueue/ArrayQueue.java
@@ -25,7 +25,7 @@ public class ArrayQueue {
     }
 
     boolean isFull() {
-        return first == 0 && last == max - 1;
+        return (first == 0 && last == max - 1) || (last + 1 == first);
     }
 
     Object front() {
@@ -48,6 +48,51 @@ public class ArrayQueue {
             first++;
         }
         return x;
+    }
+
+    boolean addCapacity() {
+        int max1 = max + 5;
+        Object[] aq1 = new Object[max1];
+        if (aq1 == null) {
+            return false;
+        }
+
+        if (first <= last) {
+            for (int i = first; i <= last; i++) {
+                aq1[i - first] = aq[i];
+            }
+        } else {
+            for (int i = first; i < max; i++) {
+                aq1[i - first] = aq[i];
+            }
+
+            for (int j = 0; j <= last; j++) {
+                aq1[max - first + j] = aq[j];
+            }
+        }
+        aq = aq1;
+        first = 0;
+        last = max - 1;
+        max = max1;
+        return true;
+    }
+
+    void enqueue(Object data) {
+        if (isFull() && !addCapacity()) {
+            return;
+        }
+
+        if (last == -1 || last == max - 1) {
+            aq[0] = data;
+            last = 0;
+
+            if (first == -1) {
+                first = 0;
+            }
+        } else {
+            last++;
+            aq[++last] = data;
+        }
     }
 
 }
